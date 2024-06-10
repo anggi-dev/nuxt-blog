34e6d69bdc772d577064fde91347253447ccac1b
anggi-dev <anggi.nuraziz1999@gmail.com>
2024-06-10 17:26:41 +0700

    alt atribut

--- pages/about.vue
+++ pages/about.vue
@@ -57,6 +57,7 @@ defineOgImageComponent('About', {
           <div class="sm:hidden block col-span-3 pb-5 dark:text-[#F1F2F4]">
             <NuxtImg
               src="/anggidev.jpeg"
+              alt="Anggi Dev"
               width="125"
               height="115"
               quality="50"
@@ -74,6 +75,7 @@ defineOgImageComponent('About', {
       <div class="hidden sm:block col-span-3">
         <NuxtImg
           src="/anggidev.jpeg"
+          alt="Anggi Dev "
           width="450"
           height="500"
           quality="50"
